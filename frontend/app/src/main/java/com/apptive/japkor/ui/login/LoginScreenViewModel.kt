28696912a6f893c97a4be3d965eb59e33d86acfb
mark <mark77234@naver.com>
2025-12-04 00:10:18 +0900

    feat: 로딩 다이얼로그 UI 개선

--- frontend/app/src/main/java/com/apptive/japkor/ui/login/LoginScreenViewModel.kt
+++ frontend/app/src/main/java/com/apptive/japkor/ui/login/LoginScreenViewModel.kt
@@ -2,9 +2,7 @@ package com.apptive.japkor.ui.login
 
 import android.app.Application
 import android.util.Log
-import androidx.datastore.dataStore
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.apptive.japkor.data.local.DataStoreManager
 import com.apptive.japkor.data.repository.AuthRepository
@@ -29,26 +27,30 @@ class LoginScreenViewModel(
 
             Log.d("LoginVM", "signIn 호출: email=$email, password=$password")
 
-            val result = repository.signIn(email, password)
-            if (result != null) {
-                Log.d("LoginVM", "로그인 성공! token=${result.token}")
+            try {
+                val result = repository.signIn(email, password)
+                if (result != null) {
+                    Log.d("LoginVM", "로그인 성공! token=${result.token}")
 
-                dataStore.saveUserInfo(
-                    memberId = result.memberId,
-                    name = result.name,
-                    token = result.token,
-                    status = result.status.name // enum → string 저장
-                )
+                    dataStore.saveUserInfo(
+                        memberId = result.memberId,
+                        name = result.name,
+                        token = result.token,
+                        status = result.status.name // enum → string 저장
+                    )
 
-                val info = dataStore.getUserInfo().first()
-                Log.d("LoginVM", "저장된 UserInfo: $info")
-                _isLoading.value = false
-                onResult(true)
-
-
-            } else {
-                Log.e("LoginVM", "로그인 실패")
+                    val info = dataStore.getUserInfo().first()
+                    Log.d("LoginVM", "저장된 UserInfo: $info")
+                    onResult(true)
+                } else {
+                    Log.e("LoginVM", "로그인 실패: 결과 null")
+                    onResult(false)
+                }
+            } catch (e: Exception) {
+                Log.e("LoginVM", "로그인 실패: 예외 발생", e)
                 onResult(false)
+            } finally {
+                _isLoading.value = false
             }
         }
     }
