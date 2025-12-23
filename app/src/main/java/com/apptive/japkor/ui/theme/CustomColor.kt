c0479784ee151a64d3f00c49cb69e13c0d41e3a8
mark <mark77234@naver.com>
2025-12-23 21:24:19 +0900

    feat: primary 색상 변경

--- app/src/main/java/com/apptive/japkor/ui/theme/CustomColor.kt
+++ app/src/main/java/com/apptive/japkor/ui/theme/CustomColor.kt
@@ -4,8 +4,12 @@ package com.apptive.japkor.ui.theme
 import androidx.compose.ui.graphics.Color
 
 object CustomColor {
-    val primary600 = Color(0xFFFF4D4D)
-    val primary300 = Color(0xFFFF9A9A)
+    val primary100 = Color(0xFFE6E9D9)
+    val primary200 = Color(0xFFD1D7B8)
+    val primary300 = Color(0xFFBBC497)
+    val primary400 = Color(0xFFA5B276)
+    val primary500 = Color(0xFF8F9F5F)
+    val primary600 = Color(0xFF7A8450)
     val gray100 = Color(0xFFF7F7F7)
     val gray200 = Color(0xFFD1D5DB)
 
