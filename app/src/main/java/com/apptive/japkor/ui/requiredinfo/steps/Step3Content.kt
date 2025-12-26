d4884074bf1339a4dd5f6aefaf1a127f3ca15ee4
mark <mark77234@naver.com>
2025-12-26 20:40:46 +0900

    fix: 인증 텍스트 제거

--- app/src/main/java/com/apptive/japkor/ui/requiredinfo/steps/Step3Content.kt
+++ app/src/main/java/com/apptive/japkor/ui/requiredinfo/steps/Step3Content.kt
@@ -82,7 +82,7 @@ fun Step3Content(
                 verticalArrangement = Arrangement.spacedBy(12.dp)
             ) {
                 CustomText(
-                    text = "학력(인증)",
+                    text = "학력",
                     type = CustomTextType.body,
                     color = CustomColor.gray300,
                     size = 12.sp
@@ -124,7 +124,7 @@ fun Step3Content(
                 verticalArrangement = Arrangement.spacedBy(12.dp)
             ) {
                 CustomText(
-                    text = "자산(인증)",
+                    text = "자산",
                     type = CustomTextType.body,
                     color = CustomColor.gray300,
                     size = 12.sp
