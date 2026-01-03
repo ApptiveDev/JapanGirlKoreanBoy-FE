7130e5e3459c4c3aec6c0a8bc6e10502f57d9171
mark <mark77234@naver.com>
2026-01-03 20:55:30 +0900

    feat: 매칭 남성 선택 api 추가

--- app/src/main/java/com/apptive/japkor/data/api/MatchingService.kt
+++ app/src/main/java/com/apptive/japkor/data/api/MatchingService.kt
@@ -3,8 +3,13 @@ package com.apptive.japkor.data.api
 import com.apptive.japkor.data.model.MatchingResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface MatchingService {
     @GET("members/matchings/female")
     fun getFemaleMatchings(): Call<List<MatchingResponse>>
+
+    @POST("members/matchings/{matchingId}/select")
+    fun selectMatching(@Path("matchingId") matchingId: Long): Call<Void>
 }
