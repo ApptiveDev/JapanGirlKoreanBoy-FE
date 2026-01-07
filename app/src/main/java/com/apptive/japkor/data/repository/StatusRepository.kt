e5396d988b4a021e7781077a47937a78d0aa1402
LEE YERAM <yeram20914@gmail.com>
2026-01-07 22:45:17 +0900

    refactor: StatusRepository.kt 제거

--- app/src/main/java/com/apptive/japkor/data/repository/StatusRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.apptive.japkor.data.repository
-
-import com.apptive.japkor.data.api.ServiceFactory
-import com.apptive.japkor.data.model.MyStatusResponse
-
-class StatusRepository {
-    suspend fun getMyStatus(): MyStatusResponse =
-        ServiceFactory.matchingService.getMyStatus()
-}
